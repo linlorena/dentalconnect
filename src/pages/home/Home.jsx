ca66cae25cc9112a371baec5e493e733630e2c3a
afgcor <annafgcorreia@gmail.com>
2025-02-26 21:53:47 -0300

    [Front-end] Telas - Inicial, Login, Cadastro

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LayoutPrincipal from "../../components/LayoutPrincipal";
+
+function Home() {
+    return (
+        <LayoutPrincipal>
+            <p>aaaaaaaa</p>
+            {/* Header do TocLoc, editar e ajustar pro layout do DentalConnect. */}
+        </LayoutPrincipal>
+    )
+};
+
+export default Home;

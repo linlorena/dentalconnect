0dfa0c3a91af2162877a049b41607cf36cbd6340
Lorena Lin <lorenalin99@gmail.com>
2025-03-27 11:34:02 -0300

    Alterações no código do FaleConosco.jsx

--- src/pages/FaleConosco/FaleConosco.jsx
+++ src/pages/FaleConosco/FaleConosco.jsx
@@ -1,47 +1,63 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import LayoutPrincipal from "../../components/LayoutPrincipal";
-import SplashHome from "../../assets/splash.png";
-import FaleConosco from "../FaleConosco/FaleConosco";
-import { FirstAid, Headphones } from "@phosphor-icons/react";
+import { Envelope, Phone, MapPin } from "@phosphor-icons/react";
 
-function Home() {
-  return (
-    <LayoutPrincipal>
-      <div className="px-16 py-10 ml-8">
-        <h1 className="text-4xl font-bold">
-          Olá, <span className="text-teal-600">usuário</span>! {/* substituir pelo nome puxado do banco de dados eventualmente */}
-        </h1>
-        <p className="text-gray-600 text-lg mt-1">
-          Como podemos cuidar da sua saúde hoje?
-        </p>
+function FaleConosco() {
+    return (
+        <LayoutPrincipal>
+            <div className="px-6 md:px-16 py-10 flex flex-col lg:flex-row items-center justify-between gap-10">
+                <div className="w-full lg:w-1/2 px-4">
+                    <h2 className="text-4xl font-bold text-sky-900 text-center md:text-left">Fale Conosco</h2>
+                    <p className="text-lg mt-4 text-center md:text-left text-sky-900">Tem dúvidas ou precisa de ajuda? Entre em contato conosco.</p>
+                    <p className="text-lg text-center md:text-left text-sky-900">Vamos fazer o possível para te ajudar. <span className="font-extrabold text-xl text-custom-teal">:)</span></p>
 
-        <div className="flex flex-col lg:flex-row items-center justify-between mt-10">
-          <img src={SplashHome} alt="Splash" className="w-full max-w-xl lg:w-1/2" />
-
-          <div className="flex flex-col lg:flex-row gap-6 lg:gap-10 mt-8 mr-8 lg:mt-0">
-            <div className="bg-custom-light rounded-2xl p-6 w-72 shadow-md hover:bg-custom-light-hover ease-in duration-150">
-                <div className="flex flex-col items-center">
-                    <FirstAid size={60} className="text-sky-900" />
-                    <h2 className="text-3xl font-bold text-custom-teal-2 mt-2">Tratamentos</h2>
+                    <div className="mt-6 space-y-4">
+                        <div className="flex items-center gap-3">
+                            <MapPin size={24} className="text-custom-teal" />
+                            <p className="text-gray-600">Avenida Santos Dumont, 3060 - Fortaleza/CE</p>
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <Phone size={24} className="text-custom-teal" />
+                            <p className="text-gray-600">(85) 98765-4321</p>
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <Envelope size={24} className="text-custom-teal" />
+                            <p className="text-gray-600">faleconosco@dentalconnect.com</p>
+                        </div>
+                    </div>
                 </div>
-                <p className="text-gray-600 mt-2 text-md leading-snug text-center">Temos um amplo cardápio de procedimentos eletivos e tratamentos disponíveis para você.</p>
-            </div>
 
-            <Link to="/fale-conosco">
-              <div className="bg-custom-light rounded-2xl p-6 w-72 shadow-md hover:bg-custom-light-hover ease-in duration-150">
-                  <div className="flex flex-col items-center">
-                      <Headphones size={60} className="text-sky-900" />
-                      <h2 className="text-3xl font-bold text-custom-teal-2 mt-2">Fale Conosco</h2>
-                  </div>
-                  <p className="text-gray-600 mt-2 text-md leading-snug text-center">A DentalConnect quer te escutar. Envie suas sugestões, reclamações ou elogios.</p>
-              </div>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </LayoutPrincipal>
-  );
+                <div className="w-full lg:w-1/2 bg-white shadow-lg rounded-lg p-6 md:p-8">
+                    <h3 className="text-3xl font-semibold text-sky-900 text-center">Envie sua mensagem</h3>
+                    <form className="mt-6 space-y-4">
+                        <input
+                            type="text" placeholder="Nome"
+                            className="w-full p-3 border border-gray-300 focus:shadow-gray-200 focus:shadow-md rounded-xl placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-custom-teal ease-in duration-150"
+                            required />
+                        <input
+                            type="email" placeholder="E-mail"
+                            className="w-full p-3 border border-gray-300 focus:shadow-gray-200 focus:shadow-md rounded-xl placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-custom-teal ease-in duration-150"
+                            required />
+                        <input
+                            type="tel" placeholder="Telefone"
+                            className="w-full p-3 border border-gray-300 focus:shadow-gray-200 focus:shadow-md rounded-xl placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-custom-teal ease-in duration-150" />
+                        <textarea
+                            rows="4" placeholder="Mensagem"
+                            className="w-full p-3 border border-gray-300 focus:shadow-gray-200 focus:shadow-md rounded-xl placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-custom-teal ease-in duration-150"
+                            required
+                        ></textarea>
+
+                        <a href="mailto:faleconosco@dentalconnect.com.br">
+                            <button
+                                type="submit" className="w-full bg-custom-teal text-sky-900 font-semibold text-xl py-3 px-6 rounded-xl hover:bg-custom-teal-hover ease-in duration-150">
+                                Enviar Mensagem
+                            </button>
+                        </a>
+                    </form>
+                </div>
+            </div>
+        </LayoutPrincipal>
+    )
 }
 
-export default Home;
+export default FaleConosco;

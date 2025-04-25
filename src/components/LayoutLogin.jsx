a883dc37d4b89a62f0cbe6b273e74cb652f3c807
Lais Barbosa <laissilva@edu.unifor.br>
2025-04-25 10:35:54 -0300

    ajustes

--- src/components/LayoutLogin.jsx
+++ src/components/LayoutLogin.jsx
@@ -8,8 +8,13 @@ function LayoutLogin({ children, fundo = false }) {
             <img src={Logo} alt="Logo DentalConnect" className="absolute top-6 right-6 w-20 md:w-24" />
 
             <div className="hidden lg:flex items-center justify-center w-1/2 bg-custom-teal rounded-r-full relative">
-                <img src={Dentista} alt="Ilustração" className="w-3/4 max-w-md" />
-            </div>
+                <img
+                    src={Dentista}
+                    alt="Ilustração"
+                    className="w-full max-w-lg lg:max-w-xl"
+                />
+             </div>
+
 
             <div className="flex flex-col justify-center items-center w-full lg:w-1/2 px-6 md:px-12">
                 {children}

bea8f749f7c40915173e040f5cae0263f6636603
afgcor <annafgcorreia@gmail.com>
2025-05-15 18:38:25 -0300

    Ajustes na padronização de ícones (Lucide > Phosphor) e exibição condicional para o tipo de conta

--- src/components/LayoutPrincipal.jsx
+++ src/components/LayoutPrincipal.jsx
@@ -5,7 +5,8 @@ import Logo from "../assets/logo_dentalconnect.png";
 import { useAuth } from '../context/auth';
 
 function LayoutPrincipal({ children }) {
-  const { logout, token } = useAuth();
+
+  const { logout, tipo, token } = useAuth();
   const navigate = useNavigate();
   const [searchTerm, setSearchTerm] = useState("");
 
@@ -21,73 +22,141 @@ function LayoutPrincipal({ children }) {
     }
   };
 
-  return (
-    <div className="h-screen flex flex-col overflow-hidden">
-      {/* Topo fixo */}
-      <div className="flex items-center justify-between px-4 py-4 bg-stone-100 border-b-4 border-teal-600 drop-shadow-sm">
-        <Link to="/home">
-          <img src={Logo} alt="Logo" className="w-28 pl-4" />
-        </Link>
-
-        {/* Envolver o input e o ícone num formulário para facilitar a submissão com Enter */}
-        <form onSubmit={handleSearchSubmit} className="w-full sm:max-w-lg lg:max-w-2xl bg-gray-200 focus-within:ring-2 focus-within:ring-custom-teal focus-within:drop-shadow-sm ease-in duration-150 rounded-full px-4 py-2 flex items-center mx-6">
-          <input
-            type="text"
-            placeholder="Encontre um profissional"
-            className="w-full bg-transparent border-none focus:outline-none placeholder:text-gray-400 text-gray-600"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <button type="submit" aria-label="Pesquisar">
-            <MagnifyingGlass size={24} className="text-gray-400 hover:text-gray-600 cursor-pointer ease-in duration-150" />
-          </button>
-        </form>
-
-        <div className="hidden sm:flex items-center gap-4 text-md">
-          <Link to="/home" className="flex items-center gap-1 font-bold hover:text-gray-700">
-            <House size={18} className="text-custom-teal" />
-            <span>Início</span>
+  if (tipo == "paciente") /* -- Exibição PACIENTE - 15.05 -- */ {
+    return (
+      <div className="h-screen flex flex-col overflow-hidden">
+        {/* Topo fixo */}
+        <div className="flex items-center justify-between px-4 py-4 bg-stone-100 border-b-4 border-teal-600 drop-shadow-sm">
+          <Link to="/home">
+            <img src={Logo} alt="Logo" className="w-28 pl-4" />
           </Link>
-          <span className="text-teal-500">|</span>
-          <Link to="/agendamentos" className="flex items-center gap-1 font-bold hover:text-gray-700">
-            <Tooth size={18} className="text-custom-teal" />
-            <span>Agendamentos</span>
-          </Link>
-          <span className="text-teal-500">|</span>
-          <Link to="/servicos" className="flex items-center gap-1 font-bold hover:text-gray-700">
-            <ShootingStar size={18} className="text-custom-teal" />
-            <span>Serviços</span>
-          </Link>
-          <span className="text-teal-500">|</span>
-          <Link to="/consultas" className="flex items-center gap-1 font-bold hover:text-gray-700">
-            <Stethoscope size={18} className="text-custom-teal" />
-            <span>Consultas</span>
-          </Link>
-          <span className="text-teal-500">|</span>
-          <Link to="/busca-avancada" className="flex items-center gap-1 font-bold hover:text-gray-700">
-            <MagnifyingGlass size={18} className="text-custom-teal" />
-            <span>Busca Avançada</span>
-          </Link>
-          <Link to="/configuracoes" className="flex items-center gap-1 font-bold hover:text-gray-700">
-            <Gear size={18} className="text-custom-teal" />
-            <span>Configurações</span>
-          </Link>
+  
+          {/* Envolver o input e o ícone num formulário para facilitar a submissão com Enter */}
+          <form onSubmit={handleSearchSubmit} className="w-full sm:max-w-lg lg:max-w-2xl bg-gray-200 focus-within:ring-2 focus-within:ring-custom-teal focus-within:drop-shadow-sm ease-in duration-150 rounded-full px-4 py-2 flex items-center mx-6">
+            <input
+              type="text"
+              placeholder="Encontre um profissional"
+              className="w-full bg-transparent border-none focus:outline-none placeholder:text-gray-400 text-gray-600"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button type="submit" aria-label="Pesquisar">
+              <MagnifyingGlass size={24} className="text-gray-400 hover:text-gray-600 cursor-pointer ease-in duration-150" />
+            </button>
+          </form>
+  
+          <div className="hidden sm:flex items-center gap-4 text-md">
+            <Link to="/home" className="flex items-center gap-1 font-bold hover:text-gray-700">
+              <House size={18} className="text-custom-teal" />
+              <span>Início</span>
+            </Link>
+            <span className="text-teal-500">|</span>
+            <Link to="/agendamentos" className="flex items-center gap-1 font-bold hover:text-gray-700">
+              <Tooth size={18} className="text-custom-teal" />
+              <span>Agendamentos</span>
+            </Link>
+            <span className="text-teal-500">|</span>
+            <Link to="/servicos" className="flex items-center gap-1 font-bold hover:text-gray-700">
+              <ShootingStar size={18} className="text-custom-teal" />
+              <span>Serviços</span>
+            </Link>
+            <span className="text-teal-500">|</span>
+            <Link to="/consultas" className="flex items-center gap-1 font-bold hover:text-gray-700">
+              <Stethoscope size={18} className="text-custom-teal" />
+              <span>Consultas</span>
+            </Link>
+            <span className="text-teal-500">|</span>
+            <Link to="/busca-avancada" className="flex items-center gap-1 font-bold hover:text-gray-700">
+              <MagnifyingGlass size={18} className="text-custom-teal" />
+              <span>Busca Avançada</span>
+            </Link>
+            <span className="text-teal-500">|</span>
+            <Link to="/configuracoes" className="flex items-center gap-1 font-bold hover:text-gray-700">
+              <Gear size={18} className="text-custom-teal" />
+              <span>Configurações</span>
+            </Link>
+          </div>
+  
+          <div className="flex items-center rounded-3xl bg-orange-500 hover:bg-orange-600 px-4 py-2 mr-4 ease-in duration-150 cursor-pointer hover:drop-shadow-xs">
+            <button 
+              onClick={handleLogout}
+              className="text-gray-900 font-bold">Sair</button>
+              <SignIn size={20} className="text-gray-900  ml-3" />
+          </div>
         </div>
-
-        <div className="flex items-center rounded-3xl bg-orange-500 hover:bg-orange-600 px-4 py-2 mr-4 ease-in duration-150 cursor-pointer hover:drop-shadow-xs">
-          <button 
-            onClick={handleLogout}
-            className="text-gray-900 font-bold">Sair</button>
-            <SignIn size={20} className="text-gray-900  ml-3" />
+  
+        {/* Conteúdo com scroll interno */}
+        <div className="flex-1 overflow-y-auto p-6 bg-white">
+          {children}
         </div>
       </div>
-
-      {/* Conteúdo com scroll interno */}
-      <div className="flex-1 overflow-y-auto p-6 bg-white">
-        {children}
+    );
+  } else {
+    return /* -- Exibição ADMIN/DENTISTA - 15.05 -- */ (
+      <div className="h-screen flex flex-col overflow-hidden">
+        {/* Topo fixo */}
+        <div className="flex items-center justify-between px-4 py-4 bg-stone-100 border-b-4 border-teal-600 drop-shadow-sm">
+          <Link to="/home">
+            <img src={Logo} alt="Logo" className="w-28 pl-4" />
+          </Link>
+  
+          {/* Envolver o input e o ícone num formulário para facilitar a submissão com Enter */}
+          <form onSubmit={handleSearchSubmit} className="w-full sm:max-w-lg lg:max-w-2xl bg-gray-200 focus-within:ring-2 focus-within:ring-custom-teal focus-within:drop-shadow-sm ease-in duration-150 rounded-full px-4 py-2 flex items-center mx-6">
+            <input
+              type="text"
+              placeholder="Encontre um profissional"
+              className="w-full bg-transparent border-none focus:outline-none placeholder:text-gray-400 text-gray-600"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button type="submit" aria-label="Pesquisar">
+              <MagnifyingGlass size={24} className="text-gray-400 hover:text-gray-600 cursor-pointer ease-in duration-150" />
+            </button>
+          </form>
+  
+          <div className="hidden sm:flex items-center gap-4 text-md">
+            <Link to="/home" className="flex items-center gap-1 font-bold hover:text-gray-700">
+              <House size={18} className="text-custom-teal" />
+              <span>Início</span>
+            </Link>
+            <span className="text-teal-500">|</span>
+            <Link to="/agendamentos" className="flex items-center gap-1 font-bold hover:text-gray-700">
+              <Tooth size={18} className="text-custom-teal" />
+              <span>Agendamentos</span>
+            </Link>
+            <span className="text-teal-500">|</span>
+            <Link to="/servicos" className="flex items-center gap-1 font-bold hover:text-gray-700">
+              <ShootingStar size={18} className="text-custom-teal" />
+              <span>Serviços</span>
+            </Link>
+            <span className="text-teal-500">|</span>
+            <Link to="/busca-avancada" className="flex items-center gap-1 font-bold hover:text-gray-700">
+              <MagnifyingGlass size={18} className="text-custom-teal" />
+              <span>Busca Avançada</span>
+            </Link>
+            <span className="text-teal-500">|</span>
+            <Link to="/configuracoes" className="flex items-center gap-1 font-bold hover:text-gray-700">
+              <Gear size={18} className="text-custom-teal" />
+              <span>Configurações</span>
+            </Link>
+          </div>
+  
+          <div className="flex items-center rounded-3xl bg-orange-500 hover:bg-orange-600 px-4 py-2 mr-4 ease-in duration-150 cursor-pointer hover:drop-shadow-xs">
+            <button 
+              onClick={handleLogout}
+              className="text-gray-900 font-bold">Sair</button>
+              <SignIn size={20} className="text-gray-900  ml-3" />
+          </div>
+        </div>
+  
+        {/* Conteúdo com scroll interno */}
+        <div className="flex-1 overflow-y-auto p-6 bg-white">
+          {children}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  
 }
 
 export default LayoutPrincipal;
